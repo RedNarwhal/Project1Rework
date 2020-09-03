b3dff95952c553ee2dae4fcd782100448e7e87c9
Josh Bertrand <joshuabertrand@protonmail.com>
2020-09-03 16:40:47 -0400

    Add some basic unit tests for Customer and Store

---
+++ ProjRework/ProjRework.Tests/CustomerTests.cs
@@ -0,0 +1,26 @@
+﻿using ProjRework.Logic.Entities;
+using System;
+using Xunit;
+
+namespace ProjRework.Tests
+{
+  public class CustomerTests
+  {
+    [Theory]
+    [InlineData("John", "Doe")]
+    public void CustomerEntityCanBeCreated(string first, string last)
+    {
+      //ARRANGE
+      var newcust = new CustomerEntity
+      {
+        FirstName = first,
+        LastName = last
+      };
+      //ACT
+      //ASSERT
+      Assert.Equal(first, newcust.FirstName);
+      Assert.Equal(last, newcust.LastName);
+    }
+
+  }
+}
---
+++ ProjRework/ProjRework.Tests/StoreTests.cs
@@ -0,0 +1,20 @@
+﻿using ProjRework.Logic.Entities;
+using System;
+using Xunit;
+
+namespace ProjRework.Tests
+{
+  public class StoreTests
+  {
+    [Theory]
+    [InlineData("Barry's Bargins")]
+    public void StoresCanBeCreated(string name)
+    {
+      var newstore = new StoreEntity
+      {
+        StoreName = name
+      };
+      Assert.Equal(name, newstore.StoreName);
+    }
+  }
+}
