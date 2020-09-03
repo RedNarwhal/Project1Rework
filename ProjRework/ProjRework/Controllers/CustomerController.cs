7e910aa6d3e93098337c5bd31c3c61b2d8429985
Josh Bertrand <joshuabertrand@protonmail.com>
2020-09-03 16:53:02 -0400

    Controllers for Customer and Store

---
+++ ProjRework/ProjRework/Controllers/CustomerController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProjRework.DataAccess.Repositories;
+using ProjRework.Logic.Entities;
+using ProjRework.Logic.Interfaces;
+
+namespace ProjRework.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class CustomerController : ControllerBase
+  {
+
+    public CustomerRepository Repo { get; }
+
+    public CustomerController(ICustomerRepository repo)
+    {
+      Repo = (CustomerRepository)(repo ?? throw new ArgumentNullException(nameof(repo)));
+    }
+
+    // GET: api/<CustomerController>
+    [HttpGet]
+    public IEnumerable<CustomerEntity> Get()
+    {
+      //Make a call to the get method of the repository. Returns a list of customers
+      var customers = Repo.Get();
+      return customers;
+    }
+
+    // GET api/<CustomerController>/5
+    [HttpGet("{id}")]
+    public CustomerEntity Get(int id)
+    {
+      var customer = Repo.Get(id);
+      return (CustomerEntity)customer;
+    }
+
+    // POST api/<CustomerController>
+    [HttpPost]
+    public void Post( string first, string last)
+    {
+      var newCust = new CustomerEntity
+      {
+        FirstName = first,
+        LastName = last
+      };
+      Repo.Insert(newCust);
+    }
+
+    // PUT api/<CustomerController>/5
+    //[HttpPut("{id}")]
+    //public void Put(int id, string value)
+    //{
+    //}
+
+    //// DELETE api/<CustomerController>/5
+    //[HttpDelete("{id}")]
+    //public void Delete(int id)
+    //{
+    //  Repo.Delete(id);
+    //}
+  }
+}
---
+++ ProjRework/ProjRework/Controllers/StoreController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProjRework.DataAccess.Repositories;
+using ProjRework.Logic.Entities;
+using ProjRework.Logic.Interfaces;
+
+namespace ProjRework.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class StoreController : ControllerBase
+  {
+    public StoreRepository Repo { get; }
+    // GET: api/<StoreController>
+    public StoreController(IStoreRepository repo)
+    {
+      Repo = (StoreRepository)(repo ?? throw new ArgumentNullException(nameof(repo)));
+    }
+    [HttpGet]
+    public List<string> Get()
+    {
+      var stores = Repo.Get();
+      return stores;
+    }
+
+    // GET api/<StoreController>/5
+    [HttpGet("{id}")]
+    public string Get(int id)
+    {
+      var store = Repo.Get(id);
+      return store.StoreName;
+    }
+
+    // POST api/<StoreController>
+    //[HttpPost]
+    //public void Post([FromBody] string value)
+    //{
+    //}
+
+    //// PUT api/<StoreController>/5
+    //[HttpPut("{id}")]
+    //public void Put(int id, [FromBody] string value)
+    //{
+    //}
+    
+  }
+}
