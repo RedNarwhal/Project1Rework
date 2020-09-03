461d373bf827b86964079553ccddfec3a70b6656
Josh Bertrand <joshuabertrand@protonmail.com>
2020-09-03 16:48:27 -0400

    Create Customer and Store Repositories for separation of concerns between DataAccess and Library logic

---
+++ ProjRework/ProjRework.DataAccess/Repositories/CustomerRepository.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using ProjRework.DataAccess.Model;
+using System.Linq;
+using ProjRework.Logic.Interfaces;
+using System.Collections;
+using ProjRework.Logic.Entities;
+
+namespace ProjRework.DataAccess.Repositories
+{
+  public class CustomerRepository : ICustomerRepository
+  {
+
+    private readonly ProjReworkContext context;
+
+    public CustomerRepository(ProjReworkContext context)
+    {
+      this.context = context;
+    }
+
+    public IEnumerable<CustomerEntity> Get()
+    {
+      // before it is returned, the Customer needs to be changed into the object version
+      var query = context.Customer.ToList();
+      var customers = new List<CustomerEntity>();
+      foreach (var item in query)
+      {
+        customers.Add(new CustomerEntity
+        {
+          Id = item.Custid,
+          FirstName = item.Firstname,
+          LastName = item.Lastname
+        });
+      }
+      return customers;
+    }
+
+    // Will throw an exception if an id that is not in the database is sent
+    public CustomerEntity Get(int id)
+    {
+      var query = context.Customer.Find(id);
+      var customer = new CustomerEntity
+      {
+        Id = query.Custid,
+        FirstName = query.Firstname,
+        LastName = query.Lastname
+      };
+      return customer;
+    }
+
+    public void Insert(CustomerEntity obj)
+    {
+      var e = new Customer
+      {
+        Firstname = obj.FirstName,
+        Lastname = obj.LastName
+      };
+      context.Add(e);
+      Save();
+    }
+    public void Update()
+    {
+      throw new NotImplementedException();
+    }
+    public void Delete(int id)
+    {
+      var customer = Get(id);
+      context.Customer.Remove(ConvertToCustomer(customer));
+      Save();
+    }
+
+    public void Save()
+    {
+      context.SaveChanges();
+    }
+
+    //Convert between Customer and Customer entity and vice-versa
+    private Customer ConvertToCustomer(CustomerEntity obj)
+    {
+      return new Customer
+      {
+        Custid = obj.CustomerId,
+        Firstname = obj.FirstName,
+        Lastname = obj.LastName
+      };
+    }
+  }
+}
---
+++ ProjRework/ProjRework.DataAccess/Repositories/StoreRepository.cs
@@ -0,0 +1,70 @@
+﻿using ProjRework.DataAccess.Model;
+using ProjRework.Logic.Interfaces;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using ProjRework.Logic.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProjRework.DataAccess.Repositories
+{
+  public class StoreRepository : IStoreRepository
+  {
+
+    public readonly ProjReworkContext context;
+    
+    public StoreRepository(ProjReworkContext context)
+    {
+      this.context = context;
+    }
+
+    public List<string> Get()
+    {
+      var query = context.Store.ToList();
+      var stores = new List<string>();
+      foreach(var item in query)
+      {
+        stores.Add(item.Storename);
+      }
+      return stores;
+    }
+    public StoreEntity Get(int id)
+    {
+      var query = context.Store.Find(id);
+      return (new StoreEntity(query.Storeid)
+      {
+        StoreName = query.Storename,
+        Inventory = GetStoreInventory(id)
+      });
+    }
+    public void Update()
+    {
+      throw new NotImplementedException();
+    }
+    public void Save()
+    {
+      context.SaveChanges();
+    }
+
+    // Get Store Inventory
+    private Dictionary<ProductEntity, double> GetStoreInventory(int storeId)
+    {
+      var invQuery = context.Storeinventory
+          .Include(i => i.Store)
+          .Where(i => i.Storeid == storeId);
+
+      var inv = new Dictionary<ProductEntity, double>();
+      foreach(var item in invQuery)
+      {
+        var prod = new ProductEntity(item.Productid)
+        {
+          ProductDescription = item.Product.Productdescription,
+          ProductPrice = (decimal)item.Product.Productprice
+        };
+        inv.Add(prod, item.Quantity);
+      }
+      return inv;
+    }
+  }
+}
